0e98267e15929fd6268e5534870fd58ff416e0f1
prithivikhatri <prithivikhatri@gmail.com>
2015-10-27 18:57:30 -0500

    started project for Lab2

---
+++ hw2-1/src/main/java/edu/mum/hw2/domain/Artist.java
@@ -0,0 +1,12 @@
+package edu.mum.hw2.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Artist {
+
+	@Id
+	Long id;
+	
+}
---
+++ hw2-1/src/main/java/edu/mum/hw2/domain/Movie.java
@@ -0,0 +1,11 @@
+package edu.mum.hw2.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+	@Id
+	Long Id;
+}
